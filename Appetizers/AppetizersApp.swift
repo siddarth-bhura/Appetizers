1e8f89bdf595eda0885d71fe4546011e7c2147ab
Siddarth Bhura <sidjain.sb@gmail.com>
2025-03-15 17:08:46 +0530

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Siddarth Bhura on 15/03/25.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
